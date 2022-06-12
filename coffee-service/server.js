8116e6527d406df949870d7356e7aa090324dee0
Odejinmi Tobiloba <odetobi123@gmail.com>
2022-06-12 19:35:25 +0100

    api gateway

--- coffee-service/server.js
+++ coffee-service/server.js
@@ -30,6 +30,17 @@ app.get('/coffee-service', (req, res) => {
    res.status(200).send(ads);
 });
 
+// custom 404
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
+})
+
+// custom error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Example app listening at http://localhost:8080`);
 });
--- server.js
+++ server.js
@@ -1,10 +1,65 @@
 "use strict"; 
-const express = require('express'); 
-const app = express(); 
+// importing the dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const createError = require('http-errors');
+const path = require('path');
+const {requestLimit} = require('./api-gateway/rateLimiter')
+const indexRouter = require('./api-gateway/AuthController');
+const port = process.env.PORT || 8080;
+// defining the Express app
+const app = express();
 
-app.get('/', (req, res) => {    
-   res.status(200).send('Hello world Tobiloba!');
-   
+// adding Helmet to enhance your API's security
+app.use(helmet());
+ 
+app.use(express.json());
+  
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
+app.use(cors());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+//10 requests per minute - rate limiting
+app.use(requestLimit);
+
+// app.get('/', (req, res) => {    
+//    res.status(200).send("Hello world Tobiloba ddd!");
+// });
+
+// app.use((req, res, next) => {
+//     console.log('Time:', Date.now())
+//     next()
+//   })
+
+app.use('/api', indexRouter);
+
+// custom 404
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+})
+  
+// Handling Errors
+app.use((err, req, res, next) => {
+    // console.log(err);
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+    res.status(err.statusCode).json({
+      message: err.message,
+    });
 });
 
-app.listen(process.env.PORT || 8080);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
